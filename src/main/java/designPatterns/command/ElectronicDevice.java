a2f271912cedb5be639a342469c4390068e11c38
tdiallo <Brasory21>
2019-11-05 09:09:50 -0500

    Commande design pattern

---
+++ src/main/java/designPatterns/command/ElectronicDevice.java
@@ -0,0 +1,13 @@
+package designPatterns.command;
+
+public interface ElectronicDevice {
+
+    public void on();
+
+    public void off();
+
+    public void volumeUp();
+
+    public void volumenDown();
+
+}
---
+++ src/main/java/designPatterns/command/TVRemote.java
@@ -0,0 +1,11 @@
+package designPatterns.command;
+
+public class TVRemote {
+
+    public static ElectronicDevice getDevice(){
+
+        return new Television();
+
+    }
+
+}

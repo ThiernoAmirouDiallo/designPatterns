179e047d2b9cd425b68dee11f68075070c622554
tdiallo <thiernoamiroud@gmail.com>
2021-01-17 23:59:45 -0500

    mediator design pattern

---
+++ src/main/java/designpatterns/mediator/JTPoorman.java
@@ -0,0 +1,12 @@
+package designpatterns.mediator;
+
+public class JTPoorman extends Colleague{
+
+    public JTPoorman(Mediator newMediator) {
+        super(newMediator);
+
+        System.out.println("JT Poorman signed up with the stockexchange\n");
+
+    }
+
+}
